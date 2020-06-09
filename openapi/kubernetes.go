bb67d27aeb25fbce11729e775d0f953f7adb583d
Stefan Lukas <afc11hn@hotmail.de>
2020-06-09 23:01:32 +0200

    Fix GB-128

--- openapi/kubernetes.go
+++ openapi/kubernetes.go
@@ -2,119 +2,12 @@ package openapi
 
 import (
 	"errors"
-	"flag"
 	appsv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/autoscaling/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	watch2 "k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/kubernetes"
-	appsV1 "k8s.io/client-go/kubernetes/typed/apps/v1"
-	coreV1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-	ptr "k8s.io/utils/pointer"
-	"path/filepath"
 )
-
-func deploymentTemplate() appsv1.Deployment {
-	return appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: "test",
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: ptr.Int32Ptr(1),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "demo",
-				},
-			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "demo",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "test",
-							Image: "nginx:1.12",
-							Ports: []apiv1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 80,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
-}
-
-func initkube() *kubernetes.Clientset {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// create the clientset
-	clientset := kubernetes.NewForConfigOrDie(config)
-	return clientset
-}
-
-type API struct {
-	Client            *kubernetes.Clientset
-	StandardNamespace string
-}
-
-func NewAPI() API {
-	client := initkube()
-	api := API{Client: client, StandardNamespace: "test"}
-	api.GetNamespaceOrDie()
-	return api
-}
-
-func (api API) GetNamespace() (*apiv1.Namespace, error) {
-	return api.GetNamespaceClient().Get(api.StandardNamespace, metav1.GetOptions{})
-}
-
-func (api API) GetNamespaceClient() coreV1.NamespaceInterface {
-	return api.Client.CoreV1().Namespaces()
-}
-
-func (api API) GetDeploymentClient() appsV1.DeploymentInterface {
-	return api.Client.AppsV1().Deployments(api.StandardNamespace)
-}
-
-func (api API) GetNamespaceOrDie() apiv1.Namespace {
-	if namespace, err := api.GetNamespace(); err != nil {
-		template := apiv1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: api.StandardNamespace,
-			},
-		}
-		if _, err := api.GetNamespaceClient().Create(&template); err != nil {
-			panic("Could not create standard namespace")
-		}
-
-		return template
-	} else {
-		return *namespace
-	}
-}
 
 func (api API) Destroy(deploymentName string) error {
 	deletePolicy := metav1.DeletePropagationForeground
